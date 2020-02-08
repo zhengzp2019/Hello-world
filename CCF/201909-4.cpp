bc14ab0a9e5f95e7707dcc7d27ee7dcefca0d6a1
Dapeng DP <44330233@qq.com>
2020-02-08 14:47:09 +0800

    add

--- CCF/201909-4.cpp
+++ CCF/201909-4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 typedef struct commodity
@@ -10,14 +11,15 @@ typedef struct commodity
     struct commodity *next;
 } commodity;
 
-int n, m, op, OPnum, OPask, type;
+//m类商品，每类商品有n个种类，cmd为命令，OPnum为op数目，查询操作用OPask个
+int n, m, cmd, OPnum, OPask, type;
 commodity co = {0, 0, NULL};
-vector<commodity> a;
+vector<commodity> a(m);
 fstream in("201909-4_in.txt");
 fstream out("201909-4_out.txt");
-void Op1(vector<commodity> &a);       //insert
-void Op2(vector<commodity> &a);       //delete
-void Op3(const vector<commodity> &a); //print information
+void Op1(vector<commodity> &a); //insert
+void Op2(vector<commodity> &a); //delete
+void Op3(vector<commodity> &a); //print information
 
 int main()
 {
@@ -26,21 +28,33 @@ int main()
     {
         a.push_back(co);
     }
-    while (n--)
+    for (int i = 0; i < n; i++)
     {
         commodity *c = (commodity *)malloc(sizeof(commodity));
         in >> c->id >> c->score;
+        c->next = NULL;
         for (int i = 0; i < m; i++)
         {
-            c->next = a[i].next;
-            a[i].next = c;
+            commodity *ci = (commodity *)malloc(sizeof(commodity));
+            *ci = *c;
+            commodity *p, *q; //p指向最小的score比c->score大的商品
+                              //q指向最大的score比c->score小的商品
+            p = &a[i];
+            q = a[i].next;
+            while (q != NULL && q->score >= c->score)
+            {
+                p = q;
+                q = q->next;
+            }
+            p->next = ci;
+            ci->next = q;
         }
     }
     in >> OPnum;
     while (OPnum--)
     {
-        in >> op;
-        switch (op)
+        in >> cmd;
+        switch (cmd)
         {
         case 1:
             Op1(a);
@@ -50,12 +64,13 @@ int main()
             break;
         case 3:
             Op3(a);
-            
             break;
         default:
             break;
         }
     }
+    in.close();
+    out.close();
     return 0;
 }
 
@@ -63,22 +78,70 @@ void Op1(vector<commodity> &a)
 {
     commodity *cc = (commodity *)malloc(sizeof(commodity));
     in >> type >> cc->id >> cc->score;
-    cc->next = a[type].next;
-    a[type].next = cc;
+    commodity *p, *q; //p指向最小的score比c->score大的商品
+                      //q指向最大的score比c->score小的商品
+    p = &a[type];
+    q = a[type].next;
+    while (q != NULL && q->score >= cc->score)
+    {
+        p = q;
+        q = q->next;
+    }
+    cc->next = q;
+    p->next = cc;
 }
 
 void Op2(vector<commodity> &a)
 {
-    in >> type >> co.id;
+    int id;
+    in >> type >> id;
+    commodity *p = &a[type], *q; //p指向编号为id的前一个商品，q指向编号为id的商品
+    q = p->next;
+    while (q != NULL && q->id != id)
+    {
+        p = q;
+        q = q->next;
+    }
+    p->next = q->next;
+    free(q);
 }
 
-void Op3(const vector<commodity> &a)
+void Op3(vector<commodity> &a)
 {
     int K;
     vector<int> opt(m);
+    vector<commodity> M(m);
     in >> K;
     for (int i = 0; i < m; i++)
-    {
         in >> opt[i];
+    for (int i = 0; i < m; i++)
+    {
+        if (K == 0)
+        {
+            out << "-1\n";
+            break;
+        }
+        vector<int> id;
+        commodity *p = a[i].next;
+        for (int j = 0; j < opt[i] && K; K--, j++)
+        {
+            id.push_back(p->id);
+            p = p->next;
+        }
+        sort(id.begin(), id.end());
+        bool first = true;
+        for (auto p = id.begin(); p != id.end(); p++)
+        {
+            if (first)
+            {
+                out << *p;
+                first = !first;
+            }
+            else
+            {
+                out << " " << *p;
+            }
+        }
+        out << endl;
     }
 }
