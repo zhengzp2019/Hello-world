9fbf806a9be7a6e3cfefaf768d260e50fd9a4ab8
Dapeng DP <44330233@qq.com>
2020-02-08 16:18:48 +0800

    add

---
+++ UVA/12657.cpp
@@ -0,0 +1,96 @@
+#include <cstdio>
+#define MAXN 100000
+using namespace std;
+FILE *fp_out = fopen("output.txt", "w");
+FILE *fp_in = fopen("input.txt", "r");
+int left[MAXN], right[MAXN];
+void swap(int L, int R);
+void Link(int L, int R);
+
+int main()
+{
+    int kase = 0, n, m;
+    int cmd;
+    while (fscanf(fp_in, "%d %d", &n, &m) == 2)
+    {
+        //链表初始化，用两个数组构建一个双向链表
+        for (int i = 1; i <= n; i++)
+        {
+            left[i] = i - 1;
+            right[i] = (i + 1) % (n + 1);
+        }
+        left[0] = n;
+        right[0] = 1;
+        int inv = 1; //0表示翻转，1表示未反转
+
+        //输入命令，并处理
+        while (m--)
+        {
+            fscanf(fp_in, "%d", &cmd);
+            if (cmd == 4) //转置整条链
+                inv = !inv;
+            else
+            {
+                int X, Y;
+                fscanf(fp_in, "%d %d", &X, &Y);
+                if (inv == 3) //交换X和Y
+                    swap(X, Y);
+                if (!inv)
+                    cmd = 3 - cmd;
+                int LX = left[X], RX = right[X], LY = left[Y], RY = right[Y];
+                if (cmd == 1) //X放在Y左边
+                {
+                    Link(LX, RX);
+                    Link(LY, X);
+                    Link(X, Y);
+                }
+                if (cmd == 2) //X放在y右边
+                {
+                    Link(LX, RX);
+                    Link(X, RY);
+                    Link(Y, X);
+                }
+            }
+        }
+
+        //求和
+        long long sum = 0;
+        if (inv) //正向
+        {
+            int R = right[0];
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                    sum += R;
+                R = right[R];
+            }
+        }
+        else //反向
+        {
+            int L = left[0];
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                    sum += L;
+                L = left[L];
+            }
+        }
+        fprintf(fp_out, "Case %d: %lld\n", kase++, sum);
+    }
+    return 0;
+}
+
+void swap(int L,int R)
+{
+    int LL = left[L], RR = right[R], LR = right[L], RL = left[R];
+    Link(LL, R);
+    Link(L, RR);
+    Link(LR, R);
+    Link(L, RL);
+}
+
+void Link(int L,int R)
+{
+    right[L] = R;
+    left[R] = L;
+}
---
+++ UVA/136.cpp
@@ -0,0 +1,33 @@
+#include <set>
+#include <queue>
+#include <iostream>
+using namespace std;
+typedef long long int LL;
+const int a[] = {2, 3, 5};
+int main()
+{
+    set<LL> s;
+    priority_queue<LL, vector<LL>, greater<LL>> pq;
+    pq.push(1);
+    s.insert(1);
+    for (size_t i = 1;; i++)
+    {
+        LL x = pq.top();
+        pq.pop();
+        if (i == 1500)
+        {
+            cout <<"The 1500'th ugly number is "<< x << endl;
+            break;
+        }
+        for (size_t i = 0; i < 3; i++)
+        {
+            LL x2 = x * a[i];
+            if (s.find(x2) == s.end())
+            {
+                pq.push(x2);
+                s.insert(x2);
+            }
+        }
+    }
+    return 0;
+}
