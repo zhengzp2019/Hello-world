6df06ffb21c73131f29ae5a0659ae319692f983f
Dapeng DP <44330233@qq.com>
2020-02-09 16:06:07 +0800

    add uva

--- UVA/679.cpp
+++ UVA/679.cpp
@@ -3,30 +3,31 @@
 #include <cstdlib>
 const int maxn = 20;
 int a[1 << maxn]; //存储每个结点的开关情况，0为关，1为开
-FILE *in = fopen("input", "r");
-FILE *out = fopen("output", "w");
 
 int main()
 {
-    int D, I;
-    while (fscanf(in, "%d%d", &D, &I) == 2)
+    int l;
+    while (scanf("%d", &l) == 1 && l != -1)
     {
-        int n = 1 << D, k = 1;
-        memset(a, 0, sizeof(a));
-        for (int i = 1; i <= I; i++)
+        int D, I;
+        while (l--)
         {
-            k = 1;
-            while (1)
+            scanf("%d%d", &D, &I);
+            int n = (1 << D) - 1, k = 1;
+            memset(a, 0, sizeof(a));
+            for (int i = 1; i <= I; i++)
             {
-                a[k] = !a[k];
-                k = a[k] ? 2 * k : 2 * k + 1;
-                if (k > n)
-                    break;
+                k = 1;
+                while (1)
+                {
+                    a[k] = !a[k];
+                    k = a[k] ? 2 * k : 2 * k + 1;
+                    if (k > n)
+                        break;
+                }
             }
+            printf("%d\n", k / 2);
         }
-        fprintf(out, "%d\n", k / 2);
     }
-    fclose(in);
-    fclose(out);
     return 0;
 }
